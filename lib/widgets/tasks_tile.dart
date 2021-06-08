8951430f217ee5933b8e912d389f0f643d19ba9e
nmoragane <nmoragane@gmail.com>
2021-06-08 23:15:20 +0530

    set check boxes

--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void checkboxChecked(bool checkboxState) {
+    setState(() {
+      isChecked = checkboxState;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Task 01'),
-      trailing: Checkbox(
-        value: true,
-        // onChanged: () {},
+      title: Text(
+        'Task 01',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: TaskCheckBox(isChecked, checkboxChecked),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function toggleCheckBoxState;
+
+  TaskCheckBox(this.checkBoxState, this.toggleCheckBoxState);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkBoxState,
+      onChanged: toggleCheckBoxState,
     );
   }
 }
